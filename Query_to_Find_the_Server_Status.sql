08689f07248fb4c3f670c1899f56170caf4d2c34
Devesh Kumar Shrivastav <unidev39@gmail.com>
2020-11-22 10:39:20 +0545

    Update Query_to_Find_the_Server_Status.sql

--- Query_to_Find_the_Server_Status.sql
+++ Query_to_Find_the_Server_Status.sql
@@ -403,3 +403,134 @@ AND owner ='RAS'
 AND table_name  IN ('<<TABLE_NAME>>') 
 AND DEGREE < 8;
 
+
+-- To find Database Up Time
+SELECT
+    'Hostname       : ' ||host_name
+    ,'Instance Name : ' ||instance_name
+    ,'Started At    : ' ||TO_CHAR(startup_time,'DD-MON-YYYY HH24:MI:SS') stime
+    ,'Uptime        : ' ||FLOOR(sysdate - startup_time)|| ' days(s) ' ||TRUNC( 24*((SYSDATE-startup_time) - TRUNC(SYSDATE-startup_time)))|| ' hour(s) ' ||MOD(TRUNC(1440*((SYSDATE-startup_time) - TRUNC(SYSDATE-startup_time))), 60)||' minute(s) '||
+    MOD(TRUNC(86400*((sysdate-startup_time) - trunc(sysdate-startup_time))), 60)||' seconds' uptime
+FROM sys.gv_$instance;
+
+
+--To Find Oracle SID based on OS PID
+SELECT 
+    b.spid,
+    a.sid,
+    a.serial#,
+    a.username,
+    a.osuser
+FROM v$session a, v$process b
+WHERE a.paddr = b.addr AND b.spid = '23924'
+ORDER BY b.spid;
+
+--To Find Oracle Sessions based on OS SID
+DECLARE
+  l_sid    NUMBER;
+  s        sys.v_$session%ROWTYPE;
+  p        sys.v_$process%ROWTYPE;
+  p_sid    NUMBER := 2187;
+BEGIN
+  BEGIN
+    SELECT sid INTO l_sid FROM sys.v_$session s WHERE  sid=p_sid;
+  EXCEPTION 
+    WHEN NO_DATA_FOUND THEN
+      dbms_output.put_line('Unable to find SID '||p_sid||'!!!');
+      RETURN;
+    WHEN OTHERS THEN
+      dbms_output.put_line(SQLERRM);
+      RETURN;
+  END;
+
+  SELECT * INTO s FROM sys.v_$session WHERE sid=l_sid;
+  SELECT * INTO p FROM sys.v_$process WHERE addr=s.paddr;
+
+  dbms_output.put_line('=====================================================================');
+  dbms_output.put_line('SID/Serial  : '|| s.sid||','||s.serial#);
+  dbms_output.put_line('Foreground  : '|| 'PID: '||s.process||' - '||s.program);
+  dbms_output.put_line('Shadow      : '|| 'PID: '||p.spid||' - '||p.program);
+  dbms_output.put_line('Terminal    : '|| s.terminal || '/ ' || p.terminal);
+  dbms_output.put_line('OS User     : '|| s.osuser||' on '||s.machine);
+  dbms_output.put_line('Ora User    : '|| s.username);
+  dbms_output.put_line('Status Flags: '|| s.status||' '||s.server||' '||s.type);
+  dbms_output.put_line('Tran Active : '|| NVL(s.taddr, 'NONE'));
+  dbms_output.put_line('Login Time  : '|| TO_CHAR(s.logon_time, 'Dy HH24:MI:SS'));
+  dbms_output.put_line('Last Call   : '|| TO_CHAR(SYSDATE-(s.last_call_et/60/60/24), 'Dy HH24:MI:SS') || ' - ' || TO_CHAR(s.last_call_et/60, '9999999999.0') || ' min');
+  dbms_output.put_line('Lock/ Latch : '|| NVL(s.lockwait, 'NONE')||'/ '||NVL(p.latchwait, 'NONE'));
+  dbms_output.put_line('Latch Spin  : '|| NVL(p.latchspin, 'NONE'));
+  dbms_output.put_line('Current SQL statement:');
+
+  FOR c1 IN (SELECT * FROM sys.v_$sqltext WHERE hash_value=s.sql_hash_value ORDER BY piece) 
+  LOOP
+    dbms_output.put_line(CHR(9)||c1.sql_text);
+  END LOOP;
+  dbms_output.put_line('Previous SQL statement:');
+  
+  FOR c1 IN (SELECT * FROM sys.v_$sqltext WHERE hash_value=s.prev_hash_value ORDER BY piece) 
+  LOOP
+    dbms_output.put_line(CHR(9)||c1.sql_text);
+  END LOOP;
+  dbms_output.put_line('Session Waits:');
+
+  FOR c1 IN (SELECT * FROM sys.v_$session_wait WHERE sid = s.sid) 
+  LOOP
+    dbms_output.put_line(CHR(9)||c1.state||': '||c1.event);
+  END LOOP;
+  dbms_output.put_line('Connect Info:');
+
+  FOR c1 IN (SELECT * FROM sys.v_$session_connect_info WHERE sid = s.sid) 
+  LOOP
+    dbms_output.put_line(CHR(9)||': '||c1.network_service_banner);
+  END LOOP;
+  dbms_output.put_line('Locks:');
+  
+  FOR c1 IN (SELECT
+                 DECODE(l.type,
+                             -- Long locks
+                             'TM', 'DML/DATA ENQ',   'TX', 'TRANSAC ENQ',
+                             'UL', 'PLS USR LOCK',
+                             -- Short locks
+                             'BL', 'BUF HASH TBL',  'CF', 'CONTROL FILE',
+                             'CI', 'CROSS INST F',  'DF', 'DATA FILE   ',
+                             'CU', 'CURSOR BIND ',
+                             'DL', 'DIRECT LOAD ',  'DM', 'MOUNT/STRTUP',
+                             'DR', 'RECO LOCK   ',  'DX', 'DISTRIB TRAN',
+                             'FS', 'FILE SET    ',  'IN', 'INSTANCE NUM',
+                             'FI', 'SGA OPN FILE',
+                             'IR', 'INSTCE RECVR',  'IS', 'GET STATE   ',
+                             'IV', 'LIBCACHE INV',  'KK', 'LOG SW KICK ',
+                             'LS', 'LOG SWITCH  ',
+                             'MM', 'MOUNT DEF   ',  'MR', 'MEDIA RECVRY',
+                             'PF', 'PWFILE ENQ  ',  'PR', 'PROCESS STRT',
+                             'RT', 'REDO THREAD ',  'SC', 'SCN ENQ     ',
+                             'RW', 'ROW WAIT    ',
+                             'SM', 'SMON LOCK   ',  'SN', 'SEQNO INSTCE',
+                             'SQ', 'SEQNO ENQ   ',  'ST', 'SPACE TRANSC',
+                             'SV', 'SEQNO VALUE ',  'TA', 'GENERIC ENQ ',
+                             'TD', 'DLL ENQ     ',  'TE', 'EXTEND SEG  ',
+                             'TS', 'TEMP SEGMENT',  'TT', 'TEMP TABLE  ',
+                             'UN', 'USER NAME   ',  'WL', 'WRITE REDO  ',
+                             'TYPE='||l.type) type,
+                 DECODE(l.lmode, 0, 'NONE', 1, 'NULL', 2, 'RS', 3, 'RX', 4, 'S',    5, 'RSX',  6, 'X', TO_CHAR(l.lmode) ) lmode,
+                 DECODE(l.request, 0, 'NONE', 1, 'NULL', 2, 'RS', 3, 'RX', 4, 'S', 5, 'RSX', 6, 'X', TO_CHAR(l.request) ) lrequest,
+                 DECODE(l.type, 'MR', o.object_name,
+                                'TD', o.object_name,
+                                'TM', o.object_name,
+                                'RW', 'FILE#='||SUBSTR(l.id1,1,3)||' BLOCK#='||SUBSTR(l.id1,4,5)||' ROW='||l.id2,
+                                'TX', 'RS+SLOT#'||l.id1||' WRP#'||l.id2,
+                                'WL', 'REDO LOG FILE#='||l.id1,
+                                'RT', 'THREAD='||l.id1,
+                                'TS', DECODE(l.id2, 0, 'ENQUEUE', 'NEW BLOCK ALLOCATION'),
+                                'ID1='||l.id1||' ID2='||l.id2) objname
+       FROM  sys.v_$lock l, dba_objects o
+       WHERE sid = s.sid
+       AND l.id1 = o.object_id ) 
+  LOOP
+     dbms_output.put_line(CHR(9)||c1.type||' H: '||c1.lmode||' R: '||c1.lrequest||' - '||c1.objname);
+  END LOOP;
+  dbms_output.put_line('=====================================================================');
+END;
+/
+
+
